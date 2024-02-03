5178b7ae4078d50c1b0bcdcd554d02eacf6d6c61
Denis Lupanov <d_lupanov@wargaming.net>
2024-02-03 20:05:32 +0100

    move from mac to normal device

---
+++ currency-exchange-service/src/main/java/com/jukeeee/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,15 @@
+package com.jukeeee.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
